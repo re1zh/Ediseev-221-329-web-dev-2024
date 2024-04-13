1804f5bc6a447b604f4b4285b314398089f0e3a5
re1zh <115718443+re1zh@users.noreply.github.com>
2024-04-13 17:15:33 +0300

    fixed phone error and cookies delete

--- lab2/app/app.py
+++ lab2/app/app.py
@@ -11,15 +11,17 @@ def index():
 
 @app.route('/headers')
 def headers():
-    return render_template('requestInfo.html', header='Загаловки', data=request.headers)
+    return render_template('requestInfo.html', header='Заголовки', data=request.headers)
 
 
 @app.route('/cookies')
 def cookies():
-    response = make_response(
-        render_template('requestInfo.html', header='cookies', data=request.cookies)
-    )
-    response.set_cookie("Pum pum", "Hello")
+    response = make_response()
+    if "Pum pum" in request.cookies:
+        response.delete_cookie("Pum pum")
+    else:
+        response.set_cookie("Pum pum", "Hello")
+    response.set_data(render_template('requestInfo.html', header='cookies', data=request.cookies))
     return response
 
 
@@ -36,25 +38,24 @@ def form():
 @app.route('/phone', methods=['GET', 'POST'])
 def phone():
     if request.method == 'GET':
-        return render_template('phone.html')
+         return render_template('phone.html')
 
-    formatted_phone_number = phone_number = request.form.get('phone_number', '')
-    symbols_to_replace = {'+', ' ', '(', ')', '-', '.'}
-    phone_number = ''.join([char for char in phone_number if char not in symbols_to_replace])
-
+    result = ""
     errors = []
+    if request.method == "POST":
+        num = request.form.get('phone_number', '')
+        cleaned_number = ''.join(filter(lambda x: x.isdigit(), num))
+        if len(cleaned_number) not in (10, 11):
+            errors.append("Неверное количество цифр.")
 
-    if not phone_number.isdigit():
-        errors.append('В номере телефона встречаются недопустимые символы.')
-    if len(phone_number) not in [10, 11]:
-        errors.append('Неверное количество цифр.')
+        if not all(char.isdigit() or char in '+()-. ' for char in num):
+            errors.append("В номере телефона встречаются недопустимые символы.")
 
-    if not errors:
-        formatted_phone_number = '8-{}-{}-{}-{}'.format(
-            phone_number[-10:-7], phone_number[-7:-4], phone_number[-4:-2], phone_number[-2:]
-        )
+        if not errors:
+            result = ('8-' + cleaned_number[-10:-7] + '-' + cleaned_number[-7:-4] + '-'
+                            + cleaned_number[-4:-2] + '-' + cleaned_number[-2:])
 
-    return render_template('phone.html', phone_number=formatted_phone_number, errors=errors)
+    return render_template('phone.html', phone_number=result, errors=errors)
 
 if __name__ == '__main__':
     app.run(debug=True)
